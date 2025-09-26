ccaad6e498c04d4d8534b6b71a630ccbcf2eae6d
Ahmad Nazar <ahmadfaraznazar@gmail.com>
2025-09-27 01:09:57 +0500

    configure cloudinary

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv/config';
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 //App Configuration
 
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
+connectCloudinary();
 
 // Middlewares
 
